bedf1229594b13c647b87130b48d8df4b0cb5665
yeji <skql4128@naver.com>
2024-07-31 15:59:26 +0900

    입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
+/* eslint-disable no-undef */
 import { useState } from 'react';
 import './App.css';
 import InputList from './component/Input';
 
 function App() {
-  const [inputList, setInputList]=useState([0])
+  const initialInputState = [{ id: 0, image: '', url: '' }];
+  const [inputList, setInputList] = useState(initialInputState);
 
-const addInput =()=>{
-  let countInput = [...inputList]
-  let counter = countInput.slice(-1)[0]
-  counter += 1
-  countInput.push(counter)
-  setInputList(countInput)
-}
+  const addInput = () => {
+    setInputList((prevList) => [
+      ...prevList,
+      {
+        id: prevList.length ? prevList[prevList.length - 1].id + 1 : 0,
+        image: '',
+        url: '',
+      },
+    ]);
+  };
+
+  const removeInput = (id) => {
+    setInputList((prevList) => prevList.filter((item) => item.id !== id));
+  };
+
+  const handleInputChange = (id, field, value) => {
+    setInputList((prevList) =>
+      prevList.map((item) =>
+        item.id === id ? { ...item, [field]: value } : item
+      )
+    );
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log();
+    console.log(inputList);
+    setInputList(initialInputState);
   };
 
   return (
     <div className='App'>
       <form onSubmit={onSubmit}>
-        <InputList inputList={inputList} setInputList={setInputList}/>
-        <button onClick={addInput}>추가</button>
+        <InputList
+          inputList={inputList}
+          removeInput={removeInput}
+          handleInputChange={handleInputChange}
+        />
+        <button type='button' onClick={addInput}>
+          추가
+        </button>
         <button type='submit'>전송</button>
       </form>
     </div>
--- src/component/Input.js
+++ src/component/Input.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
-export default function InputList({inputList, setInputList}) {
-  console.log(inputList)
-  const removeInput = (index)=>{
-    setInputList(inputList.filter((_, i)=>i !==index))
-  }
+export default function InputList({
+  inputList,
+  removeInput,
+  handleInputChange,
+}) {
   return (
     <>
-    {inputList.map((item, i)=>(
-      <div className='inputDiv' key={i}>
-        <h3>IMAGE</h3>
-        <input type='text' placeholder='이미지 링크를 입력해주세요'/>
-        <h3>URL</h3>
-        <input type='text' placeholder='링크를 입력해주세요'/>
-        <button onClick={()=>removeInput(i)}>제거</button>
-      </div>
-    ))}
+      {inputList.map(({ id, image, url }) => (
+        <div className='inputDiv' key={id}>
+          <h3>IMAGE</h3>
+          <input
+            type='text'
+            placeholder='이미지 링크를 입력해주세요'
+            name='image'
+            value={image}
+            onChange={(e) => handleInputChange(id, 'image', e.target.value)}
+          />
+          <h3>URL</h3>
+          <input
+            type='text'
+            placeholder='링크를 입력해주세요'
+            name='url'
+            value={url}
+            onChange={(e) => handleInputChange(id, 'url', e.target.value)}
+          />
+          <button type='button' onClick={() => removeInput(id)}>
+            제거
+          </button>
+        </div>
+      ))}
     </>
   );
 }
